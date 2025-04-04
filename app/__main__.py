2da959e39981e4c9aecb97fce15454c55c095c45
Dobrinya <mitin1005@gmail.com>
2025-04-04 20:20:33 +0300

    add YOLO model integration and enhance WebSocket frame processing

--- app/__main__.py
+++ app/__main__.py
@@ -1,28 +1,27 @@
-import asyncio
 import json
 
+import cv2
+import numpy as np
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from loguru import logger
 
 from app.core.config import settings
+from app.core.loader import model
 from app.utils.auth import verify_token
 
 app = FastAPI(title=settings.PROJECT)
 
-
 @app.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-
     try:
         data = await websocket.receive_text()
-
         if settings.JWT_VERIFICATION:
             token_data = json.loads(data)
             token_encoded = token_data.get("token")
-
             status, message, user_id = await verify_token(token_encoded)
+
             await websocket.send_text(json.dumps({"status": status, "message": message}))
 
             if status == "error":
@@ -31,20 +30,31 @@ async def websocket_endpoint(websocket: WebSocket):
                 return
             logger.info(f"Client connected successfully | user_id: {user_id}")
         else:
-            logger.info(f"Client connected successfully")
+            logger.info("Client connected successfully")
 
         while True:
-            frame = await websocket.receive_bytes()
-            logger.debug("Received frame")
-            result = [[10.0, 20.0, 300.0, 400.0, 0.95, 1]]
-            await websocket.send_text(json.dumps(result))
-            await asyncio.sleep(0.1)
+            frame_bytes = await websocket.receive_bytes()
+
+            image_array = np.frombuffer(frame_bytes, dtype=np.uint8)
+            image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+            results = model.predict(image)
+
+            if results and results[0].boxes.data is not None:
+                boxes = results[0].boxes.data.cpu().numpy().tolist()
+            else:
+                boxes = []
+            boxes = [[round(x, 2) for x in box] for box in boxes]
+
+            if boxes:
+                filter_label = boxes[0][5]
+                boxes = list(filter(lambda b: b[5] == filter_label, boxes))
+
+            await websocket.send_text(json.dumps(boxes))
 
     except WebSocketDisconnect:
         logger.error("WebSocket disconnected")
 
-
 if __name__ == "__main__":
     logger.info("Chroma-Worker startup")
-
     uvicorn.run(app, host=settings.HOST, port=settings.PORT)
--- app/core/loader.py
+++ app/core/loader.py
@@ -1,7 +1,19 @@
+import numpy as np
+import logging
+from loguru import logger
+
 from app.core.config import settings
+from ultralytics import YOLO
 
 PUBLIC_KEY = None
 
 if settings.JWT_VERIFICATION:
     with open(settings.PUBLISHABLE_KEY_PATH, 'r') as file:
         PUBLIC_KEY = file.read()
+
+logger.info(f"Initializing YOLO model from {settings.MODEL_PATH}")
+model = YOLO(settings.MODEL_PATH)
+logging.getLogger("ultralytics").setLevel(logging.WARNING)
+model.predict(np.zeros((1, 1, 3), dtype=np.uint8))
+logging.getLogger("ultralytics").setLevel(logging.INFO)
+logger.info("Model initialized")
